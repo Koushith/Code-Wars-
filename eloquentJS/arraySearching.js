5f210d090e125b1423248b55a06a88515802fa91
koushith <koushith97@gmail.com>
2021-07-19 22:13:08 +0530

    some and every array method

---
+++ eloquentJS/arraySearching.js
@@ -0,0 +1,33 @@
+const o = { name: 'Jerry' };
+//const arr = [1, 5, 'a', o, true, 5, [1, 2], '9'];
+
+// console.log(arr.indexOf(5)); // 5
+
+// console.log(arr.lastIndexOf("a")) //5
+
+//arr.indexOf('a'); //2
+// console.log(arr.indexOf([1, 2])); //-1
+
+// const arr = [
+//   { id: 5, name: 'Judith' },
+//   { id: 7, name: 'Francis' },
+// ];
+// let result = arr.findIndex((o) => o.name === 'Francis');
+
+// let res2 = arr.findIndex((o) => o.id == 10);
+
+// const arrfind = [
+//   { id: 5, name: 'Judith' },
+//   { id: 7, name: 'Francis' },
+// ];
+// console.log(arrfind.find((o) => o.name === 'Francis')); // returns whole obj
+
+// const nameAlpha = ['k', 'o', 'u', 's', 'h'];
+
+// console.log(
+//   nameAlpha.find((ele, index, array) => {
+//     console.log('index', index);
+//     console.log('array', array);
+//     return ele === 's';
+//   })
+// );
