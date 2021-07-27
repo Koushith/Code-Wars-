03afcaa8b456f24d51f876b435a0b0c83b7375bf
koushith <koushith97@gmail.com>
2021-07-27 20:26:24 +0530

    adds string methods

---
+++ eloquentJS/string.js
@@ -0,0 +1,28 @@
+// useful string methods
+
+// starts with - checks wtr the string starts with given char. it is case sensitive
+
+const str1 = 'Saturday night plans';
+console.log(str1.startsWith('sta')); //false
+console.log(str1.startsWith('Sa')); //true
+
+// ends with -
+
+console.log(str1.endsWith('s')); //true
+console.log(str1.endsWith('ns')); //true
+
+// includes - check if the str included or not
+
+const str = 'JavaScript is amazing';
+
+console.log(str.includes('Script')); // true
+console.log(str.includes('script')); // false
+console.log(str.includes(' ')); // true
+console.log(str.includes('array')); // false
+
+// split - split the str into array
+
+const str2 = 'JavaScript is amazing';
+const strNew = 'JavaScript-is-amazing';
+
+console.log(str2.split(' ')); // ["javascript", "is", "amaizing"]
