df2cc7789581aab176c63570836746e796e6ca53
koushith <koushith97@gmail.com>
2022-01-06 19:08:10 +0530

    adds merged arrays

---
+++ codewars/8kuy/02-reversedString.js
@@ -0,0 +1,17 @@
+/**
+ * https://www.codewars.com/kata/5168bb5dfe9a00b126000018/javascript
+ * Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+ *
+ *
+ *
+ *
+ */
+
+function solution(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(solution('hello world'));
