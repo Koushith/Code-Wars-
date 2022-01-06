ee0766676259fc1a581598a8aa458c23250ee4e2
koushith <koushith97@gmail.com>
2022-01-06 18:14:23 +0530

    adds merged arrays

--- 2022/DS/Arrays/mergeSortedArray.js
+++ 2022/DS/Arrays/mergeSortedArray.js
@@ -2,4 +2,33 @@
  * given 2 sorted array, merege them
  */
 
-function mergeArray(array1, array2) {}
+function mergeArray(array1, array2) {
+  const mergedArray = [];
+  let i = 1;
+  let j = 1;
+
+  if (array1.length == 0) {
+    return array2;
+  }
+  if (array2.length == 0) {
+    return array1;
+  }
+
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+
+  while (array1Item || array2Item) {
+    if (array2Item === undefined || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
+console.log(mergeArray([0, 3, 4, 31], [3, 4, 6, 30]));
