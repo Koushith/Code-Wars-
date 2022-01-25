0f5310992f92eb23bb449755708aa7350875b516
koushith <koushith97@gmail.com>
2022-01-25 22:11:06 +0530

    adds lookup, insert methods

--- 2022/DS/Linked List/LinkedList.impl.js
+++ 2022/DS/Linked List/LinkedList.impl.js
@@ -1,5 +1,13 @@
 // 10--->5--->16
 
+// use DRY- so create a new node and reuse that.
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
     // A node has a vaue- can be anything. and pointer to next node.
@@ -18,10 +26,7 @@ class LinkedList {
    */
   append(value) {
     // create a new node and change a pointer
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
 
     // adding next props  - --> x -->xx
     this.tail.next = newNode;
@@ -51,6 +56,53 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  /**
+   * helper method to visualize-
+   */
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  // insert at any index
+
+  insert(index, value) {
+    // check for parameters- if index> length- add it at the end.
+
+    if (index > this.length) {
+      return this.append(value);
+    }
+    //create new node
+
+    let newNode = new Node(value);
+    //find the previos node to insert.
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  //traverse or lookup
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 let myLinkedList = new LinkedList(10);
@@ -58,4 +110,8 @@ console.log(myLinkedList.append(5));
 console.log(myLinkedList.append(16));
 
 console.log(myLinkedList.prepend(1));
-console.log('My LL', myLinkedList);
+console.log('My LL', myLinkedList.printList());
+console.log('get index', myLinkedList.traverseToIndex(2));
+console.log('My LL', myLinkedList.printList());
+
+console.log(myLinkedList.insert(2, 20));
