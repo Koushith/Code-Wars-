c9eb40926f01725471d38e8b9677b43c660f11fe
koushith <koushith97@gmail.com>
2022-07-15 01:42:17 +0530

    adds concat

--- adv-random-exp/passByValue.js
+++ adv-random-exp/passByValue.js
@@ -1,7 +1,24 @@
-let a = 5;
+// let a = 5;
+// let b = a;
+
+// b++;
+
+// console.log('a', a);
+// console.log('b', b);
+// console.log('a',a)
+
+// Prove arrays are objects
+
+let a = [1, 2, 3, 4];
+console.log('a-before', a);
 let b = a;
 
-b++;
+b.push('hihi');
 
+console.log('a', a); // [1,2,3,4,hihi]  -- since it is passed by referece this should be changed;
+console.log('b', b); // [1,2,3,4,hihi]
+
+a.push('amin');
+
+console.log('b', b); //  [1,2,3,4,hihi, amin]
 console.log('a', a);
-console.log('b', b);
